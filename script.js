8fb22a0d39b93ed7161b028b496cb3cce626b047
Igor_Polyakov <itworldigpo@gmail.com>
2024-04-30 11:56:55 +0300

    деякі зміни

--- script.js
+++ script.js
@@ -1,41 +1,19 @@
 new Swiper('.swiper',{
-    // стрілки
     navigation:{
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
     },
-    // Буліти
     pagination:{
         el: '.swiper-pagination',
         clickable: true,
-        // динамічні буліти
         dynamicBullets: true,
     },
-    // авто висота слайду-якщо тексту більше у якомусь блоці
     autoHeight: true,
-    // кількість слайдів на сторінці
-    slidesPerView: 2.4,
-    // відстань від слайдерів
-    spaceBetween: 30, 
-    // безкінечний слайдер
-    loop: true,
-    
-    // Швидкість прокрутки слайда
-    speed: 700,
-    
-    
-    //     // авто прокрутка слайдів
-    // autoplay:{
-    //     // пауза між прокруткою слайдів
-    //     delay: 1000,
-    //     // зупинити на останньому слайді
-    //     stopOnLastSlide: true,
-    //     // відключити після ручного перемикання
-    //     disableOnInteraction: false,
-    // },
 
-            // Брейк поінти для слайдера (адаптив)
-            // Ширина Екрану
+    slidesPerView: 2.4,
+    spaceBetween: 30, 
+    loop: true,
+    speed: 700,
     breakpoints:{
         957: {
             slidesPerView: 2.6,
